85504a0601eed3a8f72794c5138724afc6146786
DerzoGit <journetalexandre.contact@gmail.com>
2022-02-02 00:51:42 +0100

    auth ctrl & routes

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,19 +1,39 @@
 const bcrypt = require("bcrypt");
 const db = require("../config/db");
+// const Joi = require("joi");
+// const validateRequest = require("../middleware/validate-request");
 
-async function signup(params) {
-    // validate
-    if (await db.User.findOne({ where: { email: params.email } })) {
-        throw 'Email "' + params.email + '" is already registered';
-    }
-
-    const user = new db.User(params);
+exports.signup = async (req, res, next) => {
     
-    // hash password
-    user.passwordHash = await bcrypt.hash(params.password, 10);
+    try {
+        // validate
+        // const schema = Joi.object({
+        // firstName: Joi.string().required(),
+        // lastName: Joi.string().required(),
+        // email: Joi.string().email().required(),
+        // password: Joi.string().min(6).required(),
+        // });
+        // validateRequest(req, next, schema);
 
-    // save user
-    await user.save();
+        
+        const user = await db.User.findOne({ where: { email: req.body.email }})
+        if (user) {
+            return res.status(500).json({ message: "Cet email est déjà utilisé "});
+        } else {
+            const passwordHash = await bcrypt.hash(req.body.password, 10);
+            const userData = new db.User ({
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                passwordHash: passwordHash
+            });
+
+            await userData.save();
+
+            return res.status(200).json({ message: "L'utilisateur a été créé "});
+
+        }
+    } catch (error) {
+        return res.status(400).json({ message: "Une erreur est apparue lors de l'inscription "});
+    }
 }
-
-module.exports = {signup};
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,25 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const Joi = require("joi");
-const validateRequest = require("../middleware/validate-request");
 const authCtrl = require("../controllers/auth");
 
-router.post("/signup", signupSchema, signup);
+router.post("/signup", authCtrl.signup);
 
 module.exports = router;
-
-function signup(req, res, next) {
-    authCtrl.signup(req.body)
-        .then(() => res.json({ message: 'User created' }))
-        .catch(next);
-}
-
-function signupSchema(req, res, next) {
-    const schema = Joi.object({
-        firstName: Joi.string().required(),
-        lastName: Joi.string().required(),
-        email: Joi.string().email().required(),
-        password: Joi.string().min(6).required(),
-    });
-    validateRequest(req, next, schema);
-}
