8634fa39dfad2acdcaf79cc401a106548058d779
DerzoGit <journetalexandre.contact@gmail.com>
2022-02-14 23:54:18 +0100

    add posts routes

---
+++ backend/routes/post.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const postCtrl = require("../controllers/post");
+
+router.post("/", postCtrl.createPost);
+router.put("/:id", postCtrl.updatePost);
+router.delete("/:id", postCtrl.deletePost);
+router.get("/:id", postCtrl.getOnePost);
+router.get("/", postCtrl.getAllPosts);
+
+module.exports = router;
