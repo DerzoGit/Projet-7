fe774474628883be77fb4afd8f3c1632ca9a2ebb
DerzoGit <journetalexandre.contact@gmail.com>
2022-02-14 23:53:38 +0100

    add post routes to app.js

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,13 @@ const errorHandler = require("./middleware/error-handler");
 
 
 const authRoutes = require("./routes/auth");
+const postRoutes = require("./routes/post")
 
 app.use(cors());
 app.use(express.json());
 app.use(errorHandler);
 
 app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 
 module.exports = app;
