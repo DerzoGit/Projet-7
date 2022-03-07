5fa0228fa8af8e80b2031f00b07e8f7909aed8ec
DerzoGit <journetalexandre.contact@gmail.com>
2022-03-07 22:17:22 +0100

    del error handler > app.js

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const errorHandler = require("./middleware/error-handler");
 
 
 const authRoutes = require("./routes/auth");
@@ -9,7 +8,6 @@ const postRoutes = require("./routes/post")
 
 app.use(cors());
 app.use(express.json());
-app.use(errorHandler);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
