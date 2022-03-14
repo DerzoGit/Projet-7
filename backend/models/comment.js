829821aad606cf3386053ed0161dda01d2d2a846
DerzoGit <journetalexandre.contact@gmail.com>
2022-03-14 21:01:23 +0100

    add comment models

---
+++ backend/models/comment.js
@@ -0,0 +1,23 @@
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+
+class Comment extends Model {
+    static associate(models) {
+        Comment.belongsTo(models.User, { foreignKey: "userId" });
+        Comment.belongsTo(models.Post, { foreignKey: "postId" });
+    }
+};
+
+Comment.init({
+    userId: { type: DataTypes.INTEGER, allowNull: false },
+    postId: { type: DataTypes.INTEGER, allowNull: false },
+    content: { type: DataTypes.TEXT, allowNull: false }, 
+    media: { type: DataTypes.STRING, allowNull: true, defaultValue: null }
+}, {
+    sequelize,
+    modelName: "Comment",
+    tableName: "comments",
+    timestamps: true
+});
+
+module.exports = Comment;
