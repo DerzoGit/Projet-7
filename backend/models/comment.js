843bb4f90b0bcf5169df1b16fbd535ad719ceab3
DerzoGit <journetalexandre.contact@gmail.com>
2022-03-14 22:17:47 +0100

    add on delete to comment

--- backend/models/comment.js
+++ backend/models/comment.js
@@ -3,8 +3,8 @@ const sequelize = require("../config/db");
 
 class Comment extends Model {
     static associate(models) {
-        Comment.belongsTo(models.User, { foreignKey: "userId" });
-        Comment.belongsTo(models.Post, { foreignKey: "postId" });
+        Comment.belongsTo(models.User, { foreignKey: "userId" }, { onDelete: "cascade" });
+        Comment.belongsTo(models.Post, { foreignKey: "postId" }, { onDelete: "cascade" });
     }
 };
 
