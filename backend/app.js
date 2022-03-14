eb506ce23d4f61eefd95d409e33daa1b0fd6fd6a
DerzoGit <journetalexandre.contact@gmail.com>
2022-03-14 21:01:35 +0100

    add comment routes to app.js

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,13 @@ const cors = require("cors");
 
 const authRoutes = require("./routes/auth");
 const postRoutes = require("./routes/post")
+const commentRoutes = require("./routes/comment");
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
 
 module.exports = app;
