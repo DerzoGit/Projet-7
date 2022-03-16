45ef7ec94da783aa1dff7bbb5c59b9aebbda65db
DerzoGit <journetalexandre.contact@gmail.com>
2022-03-16 11:01:19 +0100

    add app.use path to images folder

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 
 
 const authRoutes = require("./routes/auth");
@@ -9,6 +10,7 @@ const commentRoutes = require("./routes/comment");
 
 app.use(cors());
 app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
