0f98db32fe3489e8e4857aaefb1143c3fcb1ebdb
DerzoGit <journetalexandre.contact@gmail.com>
2022-03-16 11:01:30 +0100

    add multer to post routes

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const postCtrl = require("../controllers/post");
 const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
-router.post("/", auth, postCtrl.createPost);
-router.put("/:id", auth, postCtrl.updatePost);
+router.post("/", auth, multer, postCtrl.createPost);
+router.put("/:id", auth, multer, postCtrl.updatePost);
 router.delete("/:id", auth, postCtrl.deletePost);
 router.get("/:id", auth, postCtrl.getOnePost);
 router.get("/", auth, postCtrl.getAllPosts);
