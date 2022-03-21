bb7c713ade5a4b1d334349dc3d7da842edbe1ecf
DerzoGit <journetalexandre.contact@gmail.com>
2022-03-21 17:03:58 +0100

    add multer to comments routes

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const commentCtrl = require("../controllers/comment");
 const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
-router.post("/", auth, commentCtrl.createComment);
-router.put("/:id", auth, commentCtrl.updateComment);
+router.post("/", auth, multer, commentCtrl.createComment);
+router.put("/:id", auth, multer, commentCtrl.updateComment);
 router.delete("/:id", auth, commentCtrl.deleteComment);
 router.get("/:id", auth, commentCtrl.getOneComment);
 router.get("/", auth, commentCtrl.getAllComments);
