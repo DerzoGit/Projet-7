6281622ad9c202fe12b319d73bef7a69202bc3ba
DerzoGit <journetalexandre.contact@gmail.com>
2022-03-22 09:44:00 +0100

    add getAllUsers to user routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,5 +5,6 @@ const auth = require("../middleware/auth");
 
 router.delete("/:id", auth, userCtrl.deleteUser);
 router.get("/:id", auth, userCtrl.getOneUser);
+router.get("/", auth, userCtrl.getAllUsers);
 
 module.exports = router;
