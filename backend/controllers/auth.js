913690bfe67a3ef33324e716b5af7ddb214ea5f0
DerzoGit <journetalexandre.contact@gmail.com>
2022-03-30 11:30:24 +0200

    add role to token

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -55,7 +55,7 @@ exports.login = (req, res, next) => {
                 }
                 res.status(200).json({
                     userId: user.id,
-                    token: jwt.sign({ userId: user.id }, "RANDOM_TOKEN_SECRET", { expiresIn: "24h" })
+                    token: jwt.sign({ userId: user.id, role: user.role }, "RANDOM_TOKEN_SECRET", { expiresIn: "24h" })
                 });
             });
     })
