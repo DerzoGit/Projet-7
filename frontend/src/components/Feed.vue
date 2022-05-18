ef8471f73033f353aab129aad00d0aa24c129389
DerzoGit <journetalexandre.contact@gmail.com>
2022-05-18 11:19:36 +0200

    add feed

---
+++ frontend/src/components/Feed.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <div v-for="post in posts" :key="post.postId">
+            <p>{{ post.User.firstName }} {{ post.User.lastName }}</p>
+            <p>{{ post.userId }}</p>
+            <p>{{ post.title }}</p>
+            <p>{{ post.content }}</p>
+            <img :src="post.media">
+            
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Feed",
+    data() {
+        return {
+            posts: {
+                postId: "",
+                userId: "",
+                title: "",
+                content: "",
+                media: "",
+                createdAt: "",
+                pseudo: ""
+            },
+            token: localStorage.getItem("userToken")
+        }
+    },
+    mounted() {
+            this.axios.get(`http://localhost:3000/api/post/`, {
+                headers: {
+                    Authorization: `Bearer ${this.token}`
+                }
+            })
+            .then((res) => {
+                this.posts = res.data
+                console.log(res)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+}
+</script>
+
+<style>
+
+</style>
