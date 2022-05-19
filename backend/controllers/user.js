c58131b79c465bdfdb8b71a9b3597bc36153cd04
DerzoGit <journetalexandre.contact@gmail.com>
2022-05-19 07:41:15 +0200

    requests changed with models import changes

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,22 +1,23 @@
-const User = require("../models/user");
+// const User = require("../models/user");
+const db = require("../models/index");
 
 
 exports.deleteUser = (req, res, next) => {
-    User.destroy({ where: {id: req.params.id} })
+    db.User.destroy({ where: {id: req.params.id} })
     .then(() => res.status(200).json({ message: "L'utilisateur a bien été supprimé" }))
     .catch(error => res.status(400).json({ error }))
 }
 
 
 exports.getOneUser = (req, res, next) => {
-    User.findOne({ where: {id: req.params.id} })
+    db.User.findOne({ where: {id: req.params.id} })
     .then(user => res.status(200).json(user))
     .catch(error => res.status(404).json({ error }))
 }
 
 
 exports.getAllUsers = (req, res, next) => {
-    User.findAll()
+    db.User.findAll()
     .then(users => res.status(200).json(users))
     .catch(error => res.status(400).json({ error }))
 }
