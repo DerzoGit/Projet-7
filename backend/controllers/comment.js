8a88320fd480436e3cbca93301af5f6a77bb2b5b
DerzoGit <journetalexandre.contact@gmail.com>
2022-05-20 07:31:11 +0200

    requests changed with models import changes

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -1,9 +1,10 @@
-const Comment = require("../models/comment");
+// const Comment = require("../models/comment");
+const db = require("../models/index");
 const fs = require("fs");
 
 exports.createComment = (req, res, next) => {
     const commentObject = req.body;
-    const comment = new Comment({
+    const comment = new db.Comment({
         ...commentObject,
         media: (req.file ? `${req.protocol}://${req.get("host")}/images/${req.file.filename}` : null)
     });
@@ -16,14 +17,14 @@ exports.createComment = (req, res, next) => {
 exports.updateComment = (req, res, next) => {
     const userId = req.auth.userId;
 
-    Comment.findOne({ where: {id: req.params.id }})
+    db.Comment.findOne({ where: {id: req.params.id }})
     .then(comment => {
 
         if (userId === comment.userId) {
             const commentObject = req.file ? { 
                 ...req.body, media: `${req.protocol}://${req.get("host")}/images/${req.file.filename}` 
             } : { ...req.body };
-            Comment.update({ ...commentObject, id: req.params.id}, { where: {id: req.params.id} })
+            db.Comment.update({ ...commentObject, id: req.params.id}, { where: {id: req.params.id} })
             .then(() => res.status(200).json({ message: "Le commentaire a bien été modifié" }))
             .catch(error => res.status(400).json({ error }))
         } else {
@@ -38,18 +39,18 @@ exports.updateComment = (req, res, next) => {
 exports.deleteComment = (req, res, next) => {
     const userId = req.auth.userId;
     const role = req.auth.role;
-    Comment.findOne({ where: {id: req.params.id }})
+    db.Comment.findOne({ where: {id: req.params.id }})
     .then(comment => {
         if (userId === comment.userId || role === "Admin") {
             if (comment.media) {
                 const filename = comment.media.split("/images/")[1]
                 fs.unlink(`images/${filename}`, () => {
-                    Comment.destroy({ where: {id: req.params.id} })
+                    db.Comment.destroy({ where: {id: req.params.id} })
                     .then(() => res.status(200).json({ message: "Le commentaire a bien été supprimé" }))
                     .catch(error => res.status(400).json({ error }))
                 })
             } else {
-                Comment.destroy({ where: {id: req.params.id} })
+                db.Comment.destroy({ where: {id: req.params.id} })
                 .then(() => res.status(200).json({ message: "Le commentaire a bien été supprimé" }))
                 .catch(error => res.status(400).json({ error }))
             }
@@ -62,14 +63,14 @@ exports.deleteComment = (req, res, next) => {
 
 
 exports.getOneComment = (req, res, next) => {
-    Comment.findOne({ where: {id: req.params.id} })
+    db.Comment.findOne({ where: {id: req.params.id} })
     .then(comment => res.status(200).json(comment))
     .catch(error => res.status(404).json({ error }))
 }
 
 
 exports.getAllComments = (req, res, next) => {
-    Comment.findAll()
+    db.Comment.findAll()
     .then(comments => res.status(200).json(comments))
     .catch(error => res.status(400).json({ error }))
 }
