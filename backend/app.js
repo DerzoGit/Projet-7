a2e72eb7a459e0c6b95f168a0cdf3cda0ad7c144
DerzoGit <journetalexandre.contact@gmail.com>
2022-06-06 22:31:41 +0200

    add helmet to app

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require("path");
+const helmet = require("helmet");
 
 
 const authRoutes = require("./routes/auth");
@@ -11,6 +12,7 @@ const commentRoutes = require("./routes/comment");
 
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use("/api/auth", authRoutes);
