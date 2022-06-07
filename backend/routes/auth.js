f9bf4d707e7398274b1edb86c5dded7b309e53f1
DerzoGit <journetalexandre.contact@gmail.com>
2022-06-07 08:34:14 +0200

    add check authvalidator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const authCtrl = require("../controllers/auth");
+const authValid = require("../middleware/auth-validator");
 
-router.post("/signup", authCtrl.signup);
-router.post("/login", authCtrl.login);
+router.post("/signup", authValid.checkName, authValid.checkValid, authCtrl.signup);
+router.post("/login", authValid.checkValid, authCtrl.login);
 
 module.exports = router;
