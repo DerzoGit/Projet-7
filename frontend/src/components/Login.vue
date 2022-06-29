78b05118b852403d20b769147fc2e353143ec5c6
DerzoGit <journetalexandre.contact@gmail.com>
2022-06-29 22:01:32 +0200

    login & signup css

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,14 +1,21 @@
 <template>
     <div>
-        <h1>Bienvenue sur Groupomania. Connectez-vous !</h1>
-        <div>
+        <h1>Bienvenue sur Groupomania.</h1>
+        <div class="login-form">
+            <h2>Connectez-vous !</h2>
             <form>
-                <label for="email">Mail :</label>
-                <input type="email" placeholder="Mail" v-model="email">
-                <label for="password">Mot de Passe :</label>
-                <input type="password" placeholder="Mot de passe" v-model="password">
-                <button type="submit" @click.prevent="login()">Se connecter</button>
+                <div class="login-form__group">
+                    <label for="email">Mail :</label>
+                    <input type="email" placeholder="Mail" v-model="email">
+                </div>
+                <div class="login-form__group">
+                    <label for="password">Mot de Passe :</label>
+                    <input type="password" placeholder="Mot de passe" v-model="password">
+                </div>
                 <p v-if="errorMessage">{{ errorMessage }}</p>
+                <div class="login-form__group">
+                    <button type="submit" @click.prevent="login()">Se connecter</button>
+                </div>
             </form>
         </div>
     </div>
@@ -48,6 +55,79 @@
     }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+.login-form {
+    background: #292929;
+    width: 18rem;
+    border-radius: 0.4rem;
+    box-shadow: 0 0 0.5rem rgba(255, 255, 255, 0.1);
+    box-sizing: border-box;
+    margin: 6rem auto;
+    padding: 1rem;
+
+    &__group {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        margin: 0 0 1.2rem;
+        & label {
+            display: block;
+            margin: 0 0 0.7rem;
+            font-size: 0.8rem;
+            font-weight: 400;
+            line-height: 1;
+            text-transform: uppercase;
+            letter-spacing: 0.2em;
+        }
+        & input {
+            outline: none;
+            display: block;
+            width: 100%;
+            border: none;
+            border-radius: 0.4rem;
+            box-sizing: border-box;
+            padding: 0.7rem 1.2rem;
+            font-size: inherit;
+            font-weight: 500;
+            line-height: inherit;
+            transition: 0.3s ease;
+            &:focus {
+                color: rgba(0, 0, 0, 0.8);
+            }
+        }
+        & button {
+            outline: none;
+            background: #FD2D01;
+            width: 100%;
+            border: none;
+            border-radius: 0.4rem;
+            padding: 0.7rem 1.2rem;
+            color: #FFFFFF;
+            font-family: inherit;
+            font-size: inherit;
+            font-weight: 500;
+            line-height: inherit;
+            text-transform: uppercase;
+            cursor: pointer;
+            transition: 0.3s ease;
+
+            &:hover {
+            background: #FFD7D7;
+            color: black;
+        }
+        }
+    }
+
+    @media screen and (min-width: 500px) {
+        width: 30rem;
+    }
+    @media screen and (min-width: 768px) {
+        width: 40rem;
+    }
+    @media screen and (min-width: 1024px) {
+        width: 60rem;
+    }
+}
 
 </style>
