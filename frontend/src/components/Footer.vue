441620da6f1dc8f593dbfa21b37aa3a24ee3e1ec
DerzoGit <journetalexandre.contact@gmail.com>
2022-07-04 23:56:18 +0200

    add footer

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="footer">
+        {{ copyright }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Footer",
+    data() {
+        return {
+            
+        }
+    },
+    computed: {
+        copyright: () => {
+			const currentYear = new Date().getFullYear()
+
+			return `Copyright Groupomania 2022-${currentYear}`
+		}
+    }
+}
+</script>
+
+<style lang="scss">
+.footer {
+    padding: 1rem;
+    font-size: 1rem;
+}
+</style>
