79bf9e079ad8248e92726e0110d81fd34db1abb6
DerzoGit <journetalexandre.contact@gmail.com>
2022-07-12 00:09:07 +0200

    commentaire main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,21 @@
+// Import de Vue pour utiliser une application Vue
 import Vue from 'vue'
+// Import de l'App Vue
 import App from './App.vue'
 import router from './router'
+// Import d'axios pour réaliser les requêtes backend
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+// Import du css pour reset
 import '@/assets/style.css'
 
+// Appel d'axios
 Vue.use(VueAxios, axios)
 
+// Désactive tips dans la console
 Vue.config.productionTip = false
 
+// Création d'une application vue avec utilisation de router
 new Vue({
   router,
   render: h => h(App)
