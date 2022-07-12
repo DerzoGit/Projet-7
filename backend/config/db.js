767076e7c29bbb754065a40aa49fffbf213d2481
DerzoGit <journetalexandre.contact@gmail.com>
2022-07-12 12:06:03 +0200

    comment db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,8 @@
+// Import mysql et sequelize pour utilisation de la DB
 const mysql = require("mysql2");
 const Sequelize = require("sequelize");
 
+// Connexion à la DB
 const db = new Sequelize('groupomania', 'root', 'RtPassword', {
     host: "localhost",
     port: 3306,
