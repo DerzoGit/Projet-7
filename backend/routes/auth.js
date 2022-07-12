c054cba2313d62bee60f98ce8f68b1c013fa3392
DerzoGit <journetalexandre.contact@gmail.com>
2022-07-12 12:29:22 +0200

    comment authroute

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,15 @@
+// Import express
 const express = require("express");
+// Création router avec express
 const router = express.Router();
+// Import controller d'authentification
 const authCtrl = require("../controllers/auth");
+// Import middleware de validation à l'authentification
 const authValid = require("../middleware/auth-validator");
 
+// Route d'inscription de l'utilisateur
 router.post("/signup", authValid.checkName, authValid.checkValid, authCtrl.signup);
+// Route de connexion de l'utilisateur
 router.post("/login", authValid.checkValid, authCtrl.login);
 
 module.exports = router;
