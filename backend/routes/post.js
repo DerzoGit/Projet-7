7ae1d1ef2059a960b00a5bb5e723a051cea519ae
DerzoGit <journetalexandre.contact@gmail.com>
2022-07-12 12:33:10 +0200

    comment postsroute

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,13 +1,23 @@
+// Import express
 const express = require("express");
+// Création router avec express
 const router = express.Router();
+// Import controller des posts
 const postCtrl = require("../controllers/post");
+// Import middleware d'authentification
 const auth = require("../middleware/auth");
+// Import middleware multer
 const multer = require("../middleware/multer-config");
 
+// Route de création d'un post
 router.post("/", auth, multer, postCtrl.createPost);
+// Route de modification d'un post
 router.put("/:id", auth, multer, postCtrl.updatePost);
+// Route de suppression d'un post
 router.delete("/:id", auth, postCtrl.deletePost);
+// Route de récupération d'un post
 router.get("/:id", auth, postCtrl.getOnePost);
+// Route de récupération de tous les posts
 router.get("/", auth, postCtrl.getAllPosts);
 
 module.exports = router;
