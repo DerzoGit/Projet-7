22b136d69a9bd83e334e5e6aa57bcab54e7f48dd
DerzoGit <journetalexandre.contact@gmail.com>
2022-07-12 10:32:29 +0200

    comment router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,25 +1,19 @@
+// Import Vue & VueRouter
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Home from '@/views/Home.vue'
+// Import des pages d'inscription, connexion et du feed
 import SignUp from "@/components/SignUp.vue"
 import Login from "@/components/Login.vue"
 import Feed from "@/components/Feed.vue"
 
 Vue.use(VueRouter)
 
+// Déclaration des différentes pages avec leur chemin
 const routes = [
   {
     path: '/',
     name: 'Feed',
     component: Feed
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/signup',
